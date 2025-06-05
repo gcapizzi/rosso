5d7401489cd47be6f49edb3fb6c4fbef883e123b
Giuseppe Capizzi <g.capizzi@gmail.com>
2025-06-05 16:23:18 +0100

    Test formatting

--- tests/acceptance.rs
+++ tests/acceptance.rs
@@ -4,11 +4,14 @@ use anyhow::Result;
 fn test_strings() -> Result<()> {
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut con = client.get_connection()?;
+
     redis::cmd("SET").arg("my_key").arg(42).exec(&mut con)?;
     let value: i32 = redis::cmd("GET").arg("my_key").query(&mut con)?;
     assert_eq!(42, value);
+
     let new_value: i32 = redis::cmd("INCR").arg("my_key").query(&mut con)?;
     assert_eq!(43, new_value);
+
     Ok(())
 }
 
@@ -16,6 +19,7 @@ fn test_strings() -> Result<()> {
 fn test_expiration() -> Result<()> {
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut con = client.get_connection()?;
+
     redis::cmd("SET")
         .arg("my_expiring_key")
         .arg(42)
@@ -24,9 +28,12 @@ fn test_expiration() -> Result<()> {
         .exec(&mut con)?;
     let value: i32 = redis::cmd("GET").arg("my_expiring_key").query(&mut con)?;
     assert_eq!(42, value);
+
     std::thread::sleep(std::time::Duration::from_secs(2));
+
     let value: Option<i32> = redis::cmd("GET").arg("my_expiring_key").query(&mut con)?;
     assert_eq!(None, value);
+
     Ok(())
 }
 
@@ -34,6 +41,7 @@ fn test_expiration() -> Result<()> {
 fn test_parallel_connections() -> Result<()> {
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut con = client.get_connection()?;
+
     redis::cmd("SET")
         .arg("my_parallel_key")
         .arg(42)
